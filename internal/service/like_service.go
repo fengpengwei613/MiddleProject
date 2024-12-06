fe3aaa3e4f0c0a5a4f27f157f5a2377d956382b1
dmsl0503 <2950914854@qq.com>
2024-12-07 00:05:11 +0800

    Create like_service.go
    点赞收藏

---
+++ internal/service/like_service.go
@@ -0,0 +1,93 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"middleproject/internal/model"
+	"net/http"
+	"strconv"
+)
+
+// 点赞帖子
+func LikePost(c *gin.Context) {
+	uidstr := c.DefaultQuery("uid", "-1")
+	uid, err_uid := strconv.Atoi(uidstr)
+	if err_uid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的用户ID"})
+		return
+	}
+	postidstr := c.DefaultQuery("logid", "-1")
+	postid, err_pid := strconv.Atoi(postidstr)
+	if err_pid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的帖子ID"})
+		return
+	}
+	if uid == -1 || postid == -1 {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "缺少uid或logid"})
+		return
+	}
+
+	erro, msg := model.LikePost(uid, postid)
+	if erro != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"isok": false, "failreason": msg})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"isok": true, "message": msg})
+}
+
+// 收藏帖子
+func CollectPost(c *gin.Context) {
+	uidstr := c.DefaultQuery("uid", "-1")
+	uid, err_uid := strconv.Atoi(uidstr)
+	if err_uid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的用户ID"})
+		return
+	}
+	postidstr := c.DefaultQuery("logid", "-1")
+	postid, err_pid := strconv.Atoi(postidstr)
+	if err_pid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的帖子ID"})
+		return
+	}
+	if uid == -1 || postid == -1 {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "缺少uid或logid"})
+		return
+	}
+
+	erro, msg := model.CollectPost(uid, postid)
+	if erro != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"isok": false, "failreason": msg})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"isok": true, "message": msg})
+
+}
+
+// 点赞评论
+func LikeComment(c *gin.Context) {
+	uidstr := c.DefaultQuery("uid", "-1")
+	uid, err_uid := strconv.Atoi(uidstr)
+	if err_uid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的用户ID"})
+		return
+	}
+	commentidstr := c.DefaultQuery("commentid", "-1")
+	commentid, err_cid := strconv.Atoi(commentidstr)
+	if err_cid != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "无效的评论ID"})
+		return
+	}
+	if uid == -1 || commentid == -1 {
+		c.JSON(http.StatusBadRequest, gin.H{"isok": false, "failreason": "缺少uid或commentid"})
+		return
+	}
+
+	erro, msg := model.LikeComment(uid, commentid)
+	if erro != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"isok": false, "failreason": msg})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"isok": true, "message": msg})
+}
