a91172006a2d9d95df679c8a13b53fa683dc518a
dmsl0503 <2950914854@qq.com>
2024-12-15 00:30:14 +0800

    Update comment.go

--- internal/model/comment.go
+++ internal/model/comment.go
@@ -96,3 +96,152 @@ func AddReply(replyerID int, PostID int, commentID int, content string) (error, 
 	}
 	return nil, "评论创建成功", strconv.Itoa(int(replyID))
 }
+
+// 删除评论
+//假设用户表中判断是否是管理员的属性是permission，permission=1时是管理员
+func DeleteCommentByUser(commentID int, uid int, postID int) (error, string) {
+	db_link, err := repository.Connect()
+	if err != nil {
+		return err, "连接数据库失败"
+	}
+	defer db_link.Close()
+
+	var userPermission int
+	query := "SELECT permission FROM users WHERE user_id = ?"
+	err = db_link.QueryRow(query, uid).Scan(&userPermission)
+	if err != nil {
+		return err, "获取用户权限失败"
+	}
+
+	var commenterID int
+	query = "SELECT commenter_id FROM Comments WHERE comment_id = ? AND post_id = ?"
+	err = db_link.QueryRow(query, commentID, postID).Scan(&commenterID)
+	if err != nil {
+		return err, "获取评论信息失败"
+	}
+
+	if commenterID != uid && userPermission != 1 {
+		return fmt.Errorf("无权限删除该评论"), "无权限删除该评论"
+	}
+
+	tx, err_tx := db_link.Begin()
+	if err_tx != nil {
+		return err_tx, "事务开启失败"
+	}
+
+	err = deleteAllChildComments(tx, commentID)
+	if err != nil {
+		tx.Rollback()
+		return err, "删除评论的回复失败"
+	}
+	query = "DELETE FROM Comments WHERE comment_id = ?"
+	_, err = tx.Exec(query, commentID)
+	if err != nil {
+		tx.Rollback()
+		return err, "删除评论失败"
+	}
+
+	query = "UPDATE Posts SET comment_count = comment_count - 1 WHERE post_id = ?"
+	_, err = tx.Exec(query, postID)
+	if err != nil {
+		tx.Rollback()
+		return err, "更新帖子评论数量失败"
+	}
+
+	err_commit := tx.Commit()
+	if err_commit != nil {
+		tx.Rollback()
+		return err_commit, "事务提交失败"
+	}
+
+	return nil, "删除评论成功"
+}
+
+// 删除所有子评论
+func deleteAllChildComments(tx *sql.Tx, parentCommentID int) error {
+	query := "SELECT comment_id FROM Comments WHERE parent_comment_id = ?"
+	rows, err := tx.Query(query, parentCommentID)
+	if err != nil {
+		return fmt.Errorf("查询子评论失败: %v", err)
+	}
+	defer rows.Close()
+
+	var childCommentIDs []int
+	for rows.Next() {
+		var childCommentID int
+		if err := rows.Scan(&childCommentID); err != nil {
+			return fmt.Errorf("读取子评论ID失败: %v", err)
+		}
+		childCommentIDs = append(childCommentIDs, childCommentID)
+	}
+
+	if len(childCommentIDs) == 0 {
+		return nil
+	}
+	for _, childCommentID := range childCommentIDs {
+		err := deleteAllChildComments(tx, childCommentID)
+		if err != nil {
+			return err
+		}
+
+		query = "DELETE FROM Comments WHERE comment_id = ?"
+		_, err = tx.Exec(query, childCommentID)
+		if err != nil {
+			return fmt.Errorf("删除子评论失败: %v", err)
+		}
+	}
+
+	return nil
+}
+
+// 删除回复
+func DeleteReplyByUser(replyID int, uid int, postID int, commentID int) (error, string) {
+	db_link, err := repository.Connect()
+	if err != nil {
+		return err, "连接数据库失败"
+	}
+	defer db_link.Close()
+
+	var userPermission int
+	query := "SELECT permission FROM users WHERE user_id = ?"
+	err = db_link.QueryRow(query, uid).Scan(&userPermission)
+	if err != nil {
+		return err, "获取用户权限失败"
+
+	}
+	var replierID int
+	query = "SELECT commenter_id FROM comments WHERE comment_id = ? AND parent_comment_id = ? AND post_id = ?"
+	err = db_link.QueryRow(query, replyID, commentID, postID).Scan(&replierID)
+	if err != nil {
+		return err, "获取回复信息失败"
+	}
+	if replierID != uid && userPermission != 1 {
+		return fmt.Errorf("无权限删除该回复"), "无权限删除该回复"
+	}
+
+	db, err_tx := db_link.Begin()
+	if err_tx != nil {
+		return err_tx, "事务开启失败"
+	}
+
+	query = "DELETE FROM comments WHERE comment_id = ?"
+	_, err = db.Exec(query, replyID)
+	if err != nil {
+		db.Rollback()
+		return err, "删除回复失败"
+	}
+
+	query = "UPDATE comments SET reply_count = reply_count - 1 WHERE comment_id = ?"
+	_, err = db.Exec(query, commentID)
+	if err != nil {
+		db.Rollback()
+		return err, "更新评论回复数量失败"
+	}
+	err_commit := db.Commit()
+	if err_commit != nil {
+		db.Rollback()
+		return err_commit, "事务提交失败"
+	}
+
+	return nil, "删除回复成功"
+}
