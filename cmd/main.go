4e5c44da5d1f74e6b1c8cc38d361bb4ab66f2139
dmsl0503 <2950914854@qq.com>
2024-12-22 23:45:14 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -156,9 +156,6 @@ func main() {
 	//获取举报目标详情
 	r.GET("/api/adm/getreportinfo", service.GetReportInfo)
 
-	//禁言封禁用户
-	// 数据库中users表添加一行status，类型string，表示禁言还是正常的状态，例如alter table `users` add column `status` varchar(15) not null default "normal";
-	r.POST("/api/adm/gagandenclose", service.HandleMute)
 
 	// 获取某个用户的所有粉丝
 	r.GET("/api/per/attioned", service.GetFollowers)
